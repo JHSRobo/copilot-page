9de63dfc777c33388d26e2cdaa4259461ae0aa92
Caelin Sutch <caelinsutch@gmail.com>
2019-03-08 19:05:28 -0800

    DRQ Updates

--- src/src/app/services/data-models/drq.model.ts
+++ src/src/app/services/data-models/drq.model.ts
@@ -1,4 +1,4 @@
-export class DrqModel {
+export interface DrqModel {
     header: {
         frame_id: string;
         seq: number;
@@ -7,9 +7,25 @@ export class DrqModel {
             secs: number
         };
     };
-    Vin;
-    Vout;
-    Iout;
-    Pout;
-    tempurature;
+    Vin: number;
+    Vout: number;
+    Iout: number;
+    Pout: number;
+    tempurature: number;
+    dutyCycle: number;
+    switchingFrequency: number;
+    busy: boolean;
+    off: boolean;
+    vout_ov_fault: boolean;
+    iout_oc_fault: boolean;
+    vin_uv_fault: boolean;
+    temp_fault: boolean;
+    cml_fault: boolean;
+    vout_fault: boolean;
+    iout_fault: boolean;
+    input_fault: boolean;
+    pwr_gd: boolean;
+    fan_fault: boolean;
+    other: boolean;
+    unknown: boolean;
 }
