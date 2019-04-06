a22df3a7fbbfa2c8bd812ad912a914465daefb98
Alden Parker <parkera21@student.jhs.net>
2019-04-06 14:53:18 -0700

    Timer mappings

--- src/src/app/copilot/copilot-components/timer/timer.component.ts
+++ src/src/app/copilot/copilot-components/timer/timer.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {fromEvent} from 'rxjs';
 
 @Component({
   selector: 'app-timer-module',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.css']
 })
-export class TimerComponent {
+export class TimerComponent implements OnInit {
 
   name = 'Stopwatch';
   seconds = 0;
   minutes = 0;
   timerInterval;
   stopped = true;
+
+  keyPress(event) {
+      if (event.key == 't') {
+          this.startTimer();
+      } else if (event.key == 'y') {
+          this.stopTimer();
+          this.resetTimer();
+      } else {
+      }
+  }
 
   startTimer() {
     // const start = Date.now();
@@ -27,13 +38,18 @@ export class TimerComponent {
   }
 
   resetTimer() {
-    this.seconds = '00';
-    this.minutes = '00';
+    this.seconds = 0;
+    this.minutes = 0;
   }
 
   stopTimer() {
     clearInterval(this.timerInterval);
     this.stopped = true;
+  }
+
+  ngOnInit() {
+    // Creates and subscribes too an observable that listens for key presses. Callback function runs the keypress function
+    fromEvent(document, 'keyup').pipe().subscribe(character => this.keyPress(character));
   }
 
 }
