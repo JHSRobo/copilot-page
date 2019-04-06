e87c72069c7c9f393eff77a488cf1a3ac20c5ff1
Alden Parker <parkera21@student.jhs.net>
2019-04-06 14:23:17 -0700

    YA yeet it will work now

--- src/src/app/copilot/copilot-components/thruster-sensitivity/thruster-sensitivity.component.ts
+++ src/src/app/copilot/copilot-components/thruster-sensitivity/thruster-sensitivity.component.ts
@@ -23,17 +23,17 @@ export class ThrusterSensitivityComponent implements OnInit {
   };
 
   keyPress(event) {
-      if (event.key == '/' && event.ctrlKey == true) {
+      if (event.key == 'ArrowRight' && event.ctrlKey == true) {
         this.sensitivity.l_scale = this.sensitivity.l_scale + 0.05;
-      } else if (event.key == '.' && event.ctrlKey == true) {
+      } else if (event.key == 'ArrowLeft' && event.ctrlKey == true) {
         this.sensitivity.l_scale = this.sensitivity.l_scale - 0.05;
-      } else if (event.key == '/' && event.altKey == true) {
+      } else if (event.key == 'ArrowUp' && event.ctrlKey == true) {
         this.sensitivity.v_scale = this.sensitivity.l_scale + 0.05;
-      } else if (event.key == '.' && event.altKey == true) {
+      } else if (event.key == 'ArrowDown' && event.ctrlKey == true) {
         this.sensitivity.v_scale = this.sensitivity.l_scale - 0.05;
-      } else if (event.key == '/' && event.shiftKey == true) {
+      } else if (event.key == '/' && event.ctrlKey == true) {
         this.sensitivity.a_scale = this.sensitivity.l_scale + 0.05;
-      } else if (event.key == '.' && event.shiftKey == true) {
+      } else if (event.key == '.' && event.ctrlKey == true) {
         this.sensitivity.a_scale = this.sensitivity.l_scale - 0.05;
       } else {
       }
