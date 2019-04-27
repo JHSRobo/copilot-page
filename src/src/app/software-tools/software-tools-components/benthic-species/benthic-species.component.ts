0adc76b68f95bac250e62f65eb49f4939dd4c8b1
Caelin Sutch <caelinsutch@gmail.com>
2019-04-27 11:13:37 -0700

    Integrated into webpage

--- src/src/app/software-tools/software-tools-components/benthic-species/benthic-species.component.ts
+++ src/src/app/software-tools/software-tools-components/benthic-species/benthic-species.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ShapeDetectService} from '../../../services/subscribers/shape-detect.service';
+import {ShapeDetectModel} from '../../../services/data-models/shape-detect.model';
 
 @Component({
   selector: 'app-benthic-species',
@@ -7,11 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BenthicSpeciesComponent implements OnInit {
   name = 'Benthic Species';
+  circles: number;
+  triangles: number;
+  square: number;
+  rectangle: number;
 
-
-  constructor() { }
+  constructor(
+      private shapeDetection: ShapeDetectService
+  ) { }
 
   ngOnInit() {
+    this.shapeDetection.initialize();
+    this.shapeDetection.getData().subscribe((msg: ShapeDetectModel) => {
+      try {
+        this.circles = msg.circles;
+        this.triangles = msg.triangles;
+        this.square = msg.square;
+        this.rectangle = msg.rectangle;
+      } catch (error) { }
+    });
   }
-
 }
